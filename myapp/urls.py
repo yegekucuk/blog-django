f434ae418cc21c26ea2b943cd486692606782646
yegekucuk <eegekucuk@hotmail.com>
2024-06-05 10:24:10 +0300

    Fixes in iFrame
    Fixed hobbies in iFrame

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('gallery/', views.gallery, name='gallery'),
     path('posts/', views.posts, name='posts'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('submit-review/', views.submit_review, name='submit_review')
+    path('submit-review/', views.submit_review, name='submit_review'),
+    path('hobbies/standart/', views.standart, name='standart'),
+    path('hobbies/calisthenics/', views.calisthenics, name='calisthenics'),
+    path('hobbies/swimming/', views.swimming, name='swimming'),
+    path('hobbies/basketball/', views.basketball, name='basketball')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,8 @@ from .forms import ReviewForm
 
 # Create your views here.
 def home(request):
-    return render(request, "./html/index.html")
+    reviews = Review.objects.all().order_by('-date')
+    return render(request, "./html/index.html", {'reviews': reviews})
 
 def about(request):
     return render(request, './html/about.html')
@@ -20,8 +21,7 @@ def gallery(request):
 
 def posts(request):
     posts_list = Post.objects.all()
-    reviews = Review.objects.all().order_by('-date')
-    return render(request, './html/posts.html', {'posts': posts_list, 'reviews': reviews})
+    return render(request, './html/posts.html', {'posts': posts_list})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
@@ -36,3 +36,18 @@ def submit_review(request):
     else:
         form = ReviewForm()
     return render(request, 'html/submit_review.html', {'form': form})
+
+
+
+# Hobbies
+def standart(request):
+    return render(request, './html/hobbies/standart.html')
+
+def calisthenics(request):
+    return render(request, './html/hobbies/calisthenics.html')
+
+def swimming(request):
+    return render(request, './html/hobbies/swimming.html')
+
+def basketball(request):
+    return render(request, './html/hobbies/basketball.html')
