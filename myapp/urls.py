31bde12e1a10a2ca7b23b720ecd242e649d40fb6
yegekucuk <eegekucuk@gmail.com>
2024-08-15 17:19:34 +0300

    remove all urls except main and about : test

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,14 +4,14 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('hobbies/', views.hobbies, name='hobbies'),
-    path('contact/', views.contact, name='contact'),
-    path('gallery/', views.gallery, name='gallery'),
-    path('posts/', views.posts, name='posts'),
-    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('submit-review/', views.submit_review, name='submit_review'),
-    path('hobbies/standart/', views.standart, name='standart'),
-    path('hobbies/calisthenics/', views.calisthenics, name='calisthenics'),
-    path('hobbies/swimming/', views.swimming, name='swimming'),
-    path('hobbies/basketball/', views.basketball, name='basketball')
+    #path('hobbies/', views.hobbies, name='hobbies'),
+    #path('contact/', views.contact, name='contact'),
+    #path('gallery/', views.gallery, name='gallery'),
+    #path('posts/', views.posts, name='posts'),
+    #path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    #path('submit-review/', views.submit_review, name='submit_review'),
+    #path('hobbies/standart/', views.standart, name='standart'),
+    #path('hobbies/calisthenics/', views.calisthenics, name='calisthenics'),
+    #path('hobbies/swimming/', views.swimming, name='swimming'),
+    #path('hobbies/basketball/', views.basketball, name='basketball')
 ]
